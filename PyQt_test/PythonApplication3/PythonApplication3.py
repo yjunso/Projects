3b6666a3e3f2d1c42086022acd2038ac29c04498
yjunso <yjun.so@gmail.com>
2016-03-03 17:49:20 +0900

    UI design

--- PyQt_test/PythonApplication3/PythonApplication3.py
+++ PyQt_test/PythonApplication3/PythonApplication3.py
@@ -8,26 +8,39 @@ class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("PyStock")
-        self.setGeometry(300,300,300,150)
+        self.setGeometry(300,300,300,300)
+
+        self.text_status = QTextEdit(self)
+        self.text_status.setGeometry(150, 20, 140, 30)
+        self.text_status.setEnabled(False)
+
+        self.text_edit = QTextEdit(self)
+        self.text_edit.setGeometry(10, 60, 280, 210)
+        self.text_edit.setEnabled(False)
 
         self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
+
+        if self.kiwoom.dynamicCall("GetConnectState()") == 0:
+            self.text_status.append("Not Connected")
 
         btn1 = QPushButton("Login", self)
         btn1.move(20,20)
         btn1.clicked.connect(self.btn1_clicked)
 
-        btn2 = QPushButton("Check state", self)
-        btn2.move(20,70)
-        btn2.clicked.connect(self.btn2_clicked)
+
 
     def btn1_clicked(self):
-        ret = self.kiwoom.dynamicCall("CommConnect()")
+        self.kiwoom.dynamicCall("CommConnect()")
+        self.kiwoom.connect(self.kiwoom, SIGNAL("OnEventConnect(int)"), self.OnEventConnect)
 
-    def btn2_clicked(self):
         if self.kiwoom.dynamicCall("GetConnectState()") == 0:
             self.statusBar().showMessage("Not Connected")
         else:
             self.statusBar().showMessage("Connected")
+
+    def OnEventConnect(self, ErrCode):
+        if ErrCode == 0:
+            self.text_edit.append("로그인 성공")
         
 if __name__ == "__main__":
     app = QApplication(sys.argv)
