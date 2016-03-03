72b0073e8e9aa3d83d9d6b7ae4bc4f48a437aab1
yjunso <yjun.so@gmail.com>
2016-03-03 16:27:15 +0900

    modified

--- PyQt_test/PythonApplication3/PythonApplication3.py
+++ PyQt_test/PythonApplication3/PythonApplication3.py
@@ -1,37 +1,36 @@
 ﻿import sys
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
-from PyQt4.QAxContainer  import *
+from PyQt4.QAxContainer import *
 
 
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("PyStock")
-        self.setGeometry(300,300,300,400)
+        self.setGeometry(300,300,300,150)
 
-        btn1 = QPushButton("Click me", self)
+        self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
+
+        btn1 = QPushButton("Login", self)
         btn1.move(20,20)
         btn1.clicked.connect(self.btn1_clicked)
 
-        btn2 = QPushButton("Click me too", self)
-        btn2.move(40,40)
+        btn2 = QPushButton("Check state", self)
+        btn2.move(20,70)
         btn2.clicked.connect(self.btn2_clicked)
 
     def btn1_clicked(self):
-        QMessageBox.about(self, "message", "clicked")
-    
+        ret = self.kiwoom.dynamicCall("CommConnect()")
+
     def btn2_clicked(self):
-        QMessageBox.about(self, "message", "clicked")
+        if self.kiwoom.dynamicCall("GetConnectState()") == 0:
+            self.statusBar().showMessage("Not Connected")
+        else:
+            self.statusBar().showMessage("Connected")
         
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     myWindow = MyWindow()
     myWindow.show()
     app.exec_()
-
-app = QApplication(sys.argv)
-print(sys.argv)
-label = QLabel("Hello PyQt")
-label.show()
-app.exec_()
